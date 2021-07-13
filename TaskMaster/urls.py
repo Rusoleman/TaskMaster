ad12611f1c0b8d09d11161321e9f0d20593a86a4
Rusoleman <example@mail.com>
2021-07-13 13:04:04 -0500

    Update url's

--- TaskMaster/urls.py
+++ TaskMaster/urls.py
@@ -17,14 +17,13 @@ from django.contrib import admin
 from django.urls import path
 
 from rest_framework.routers import DefaultRouter
+router = DefaultRouter()
 
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-
-router = DefaultRouter()
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     # ══════════════ URL's JWT ════════════════
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh')
-]
+] + router.urls
